0593e8714e8788ebe76602312b22e72a873f4c4f
jul <y.trubitsyna@g.nsu.ru>
2021-12-04 22:54:18 +0700

    add convert to proto Coord class

--- snake/src/main/java/com/lab/trubitsyna/snake/model/Point.java
+++ snake/src/main/java/com/lab/trubitsyna/snake/model/Point.java
@@ -1,5 +1,6 @@
 package com.lab.trubitsyna.snake.model;
 
+import com.lab.trubitsyna.snake.protoClass.SnakesProto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,10 @@ public class Point {
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public SnakesProto.GameState.Coord convertPointToCoord() {
+        return SnakesProto.GameState.Coord.newBuilder().setX(x).setY(y).build();
     }
 
 }
