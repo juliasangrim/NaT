9763d34e40fda5778b1a006c8cdd1e740f35a53f
jul <y.trubitsyna@g.nsu.ru>
2021-12-10 23:24:43 +0700

    make class node and masternode for net communication

---
+++ snake/src/main/java/com/lab/trubitsyna/snake/backend/node/MasterNode.java
@@ -0,0 +1,16 @@
+package com.lab.trubitsyna.snake.backend.node;
+
+import com.lab.trubitsyna.snake.backend.protoClass.SnakesProto;
+
+import java.net.SocketException;
+
+public class MasterNode extends Node {
+
+   public MasterNode() {
+       super();
+
+   }
+
+
+
+}
