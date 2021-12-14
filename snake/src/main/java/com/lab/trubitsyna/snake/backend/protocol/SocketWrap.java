2c2c997d99339c9ceb59ef68be7dcf6c47569761
jul <y.trubitsyna@g.nsu.ru>
2021-12-15 02:50:49 +0700

    add check socket null

--- snake/src/main/java/com/lab/trubitsyna/snake/backend/protocol/SocketWrap.java
+++ snake/src/main/java/com/lab/trubitsyna/snake/backend/protocol/SocketWrap.java
@@ -1,5 +1,6 @@
 package com.lab.trubitsyna.snake.backend.protocol;
 
+import com.lab.trubitsyna.snake.MyLogger;
 import com.lab.trubitsyna.snake.backend.protoClass.SnakesProto;
 import lombok.Getter;
 import org.slf4j.Logger;
@@ -9,7 +10,7 @@ import java.io.IOException;
 import java.net.*;
 
 public class SocketWrap implements IOWrap {
-    private final Logger logger = LoggerFactory.getLogger("APP");
+   // private final Logger logger = LoggerFactory.getLogger("APP");
     private static final int MAX_SIZE = 8192;
     @Getter
     private DatagramSocket socket;
@@ -22,10 +23,10 @@ public class SocketWrap implements IOWrap {
     @Override
     public void send(SnakesProto.GameMessage message, String receiver, int receiverPort) {
         try {
-            logger.info("Creating packet...");
+         //   logger.info("Creating packet...");
             InetAddress addr = InetAddress.getByName(receiver);
             var packet = new DatagramPacket(message.toByteArray(), message.getSerializedSize(), addr, receiverPort);
-            logger.info("Send to " + addr + ", "+ receiverPort);
+            MyLogger.getLogger().info("Send to " + addr + ", "+ receiverPort +  " " + message.getTypeCase());
             socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
@@ -34,15 +35,24 @@ public class SocketWrap implements IOWrap {
 
     @Override
     public GameMessageWrap receive() {
+        if (socket == null) {
+            MyLogger.getLogger().info("SOCKET IS NULL!!!");
+            return null;
+        }
         try {
             byte[] receiveRowByte = new byte[MAX_SIZE];
             DatagramPacket packet = new DatagramPacket(receiveRowByte, receiveRowByte.length);
+            MyLogger.getLogger().info(String.format("Trying receive message... on port=%d, addr=%s", socket.getLocalPort(), socket.getLocalAddress()));
             socket.receive(packet);
+            MyLogger.getLogger().info(String.format("Receive message... on port=%d, addr=%s", socket.getLocalPort(), socket.getLocalAddress()));
             //get the message from array(bc packet.getData() return all bytes(include zero bytes))
             byte[] receivedBytes = new byte[packet.getLength()];
             System.arraycopy(receiveRowByte, 0, receivedBytes, 0, packet.getLength());
-
-            return new GameMessageWrap(SnakesProto.GameMessage.parseFrom(receivedBytes), packet.getAddress().getHostAddress(), packet.getPort());
+            var message = SnakesProto.GameMessage.parseFrom(receivedBytes);
+            MyLogger.getLogger().info("Get message from " + packet.getAddress() + " " + packet.getPort() + " " + message.getTypeCase());
+            return new GameMessageWrap(message, packet.getAddress().getHostAddress(), packet.getPort());
+        } catch (SocketTimeoutException e) {
+            MyLogger.getLogger().info("Socket timeout");
         } catch (IOException e) {
             e.printStackTrace();
         }
