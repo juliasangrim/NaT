46efb094bdeb09ded79a36261adb9c53b2e7955c
jul <y.trubitsyna@g.nsu.ru>
2021-12-15 02:48:17 +0700

    add method addNewPlayer

--- snake/src/main/java/com/lab/trubitsyna/snake/model/IModel.java
+++ snake/src/main/java/com/lab/trubitsyna/snake/model/IModel.java
@@ -1,11 +1,13 @@
 package com.lab.trubitsyna.snake.model;
 
+import com.lab.trubitsyna.snake.backend.protoClass.SnakesProto;
 import com.lab.trubitsyna.snake.gameException.GameException;
 import com.lab.trubitsyna.snake.view.IView;
 
 public interface IModel {
     void startGame() throws GameException;
     void updateModel() throws GameException;
+    boolean addNewPlayer(SnakesProto.GamePlayer player);
     void notifyListeners() throws GameException;
     void addListener(IListener listener) throws GameException;
     void removeListener(IListener listener);
